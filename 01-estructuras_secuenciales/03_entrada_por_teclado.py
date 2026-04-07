41d74c1893d9687e1026d3b6cd254d17c678ccd3
santtu23 <santisecjalil12@gmail.com>
2026-04-06 21:57:44 -0300

    Se realiza un cambio en la validacion de datos del usuario

--- 01-estructuras_secuenciales/03_entrada_por_teclado.py
+++ 01-estructuras_secuenciales/03_entrada_por_teclado.py
@@ -1,4 +1,18 @@
-nombre = input('Ingrese su nombre: ') ## input() reconoce entrada por teclado
-edad = int(input('Ingrese su edad: '))
-print(nombre.capitalize())
-print(edad)
+while True:
+
+    nombre = input("Ingrese su nombre: ").strip().capitalize()
+    if nombre.isalpha():
+        break
+    else:
+        print("NOMBRE IVALIDO")
+
+while True:
+    
+    edad = input("Ingrese su edad como numero entero: ").strip()
+    if edad.isdigit() and len(edad) <= 2:
+        break
+    else:
+        print("EDAD INVALIDA O MAYOR A 2 DIGITOS")
+
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad}")
