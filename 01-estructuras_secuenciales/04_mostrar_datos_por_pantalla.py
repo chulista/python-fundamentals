6fb444b020ef0da153fd3a5888152e0936aed3e1
santtu23 <santisecjalil12@gmail.com>
2026-04-08 16:20:56 -0300

    Se realiza el ejercicio 2 de Mostrar por pantalla

--- 01-estructuras_secuenciales/04_mostrar_datos_por_pantalla.py
+++ 01-estructuras_secuenciales/04_mostrar_datos_por_pantalla.py
@@ -1,3 +1,5 @@
+#Ejercicio 1 de Mostrar datos por pantalla
+
 vocal = input("Ingrese una vocal en minuscula: ")
 letra_m = input("Ingrese una letra en mayuscuala: ")
 
@@ -7,3 +9,18 @@ paso_mi = letra_m.lower()
 mostrar = paso_ma + " " + paso_mi
 
 print(mostrar)
+
+print("=============================")
+
+#Ejercicio 2 
+
+nombre = input("Ingrese su nombre: ").strip().capitalize()
+edad = int(input("Ingrese su edad: "))
+genero = input("Ingrese su sexo: ").strip().capitalize()
+print()
+
+print("Te llamas:",nombre,"\n")
+print("Tu edad es:", edad,"\n")
+print("Eres:", genero)
+
+print()
