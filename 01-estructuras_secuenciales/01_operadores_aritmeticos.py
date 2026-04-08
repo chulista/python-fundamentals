4d7c12129800dfa9f158cf45d230e83b5f1caa42
santtu23 <santisecjalil12@gmail.com>
2026-04-08 15:16:39 -0300

    Se resuelve el ejerecicio de la jugueteria

--- 01-estructuras_secuenciales/01_operadores_aritmeticos.py
+++ 01-estructuras_secuenciales/01_operadores_aritmeticos.py
@@ -1,2 +1,17 @@
+#Ejercicio 1
+
 cuenta = ((3+2) / (2*5)) ** 2 
-print(cuenta)
+print("El resultado de la cuenta es:",cuenta)
+
+print("=================================")
+
+#Ejercicio 2
+
+payasos = 23
+munecas = 54
+
+peso_payaso = 112
+peso_muneca = 75
+
+total = (peso_payaso * payasos) + (peso_muneca * munecas)
+print("Peso total de los juguetes es",total,"gramos")
