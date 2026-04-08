78df98164ac836672bc8e705f31af89414769b7e
santtu23 <santisecjalil12@gmail.com>
2026-04-08 18:27:34 -0300

    Se realizan los primeros ejercicios de Live Coding

--- 01-estructuras_secuenciales/01_operadores_aritmeticos.py
+++ 01-estructuras_secuenciales/01_operadores_aritmeticos.py
@@ -1,20 +1,2 @@
-## suma
-print('suma: 10+1 =', (10+1))
-## resta
-print('resta: 10-1 =', (10-1))
-## multiplicacion
-print('multiplicacion: 10*2 =', (10*2))
-## division
-print('division: 10/3 =', (10/3))
-## division entera
-print('division entera: 10//3 =', (10//3))
-## modulo
-print('resto: 10%3 =', (10%3))
-## potencia
-print('potencia: 10**3 =', (10**3))
-
-## orden de operadores
-## parentesis
-## potencia
-## multiplicacion / division
-## suma / resta
+cuenta = ((3+2) / (2*5)) ** 2 
+print(cuenta)
