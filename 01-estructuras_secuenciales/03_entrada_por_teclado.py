944079df327c6ffa462538deae8bfe043cb4ab3d
santtu23 <santisecjalil12@gmail.com>
2026-04-08 16:02:49 -0300

    Se realiza el ejercicio 2 de Entradas por teclado

--- 01-estructuras_secuenciales/03_entrada_por_teclado.py
+++ 01-estructuras_secuenciales/03_entrada_por_teclado.py
@@ -12,4 +12,15 @@ print(resolucion)
 
 print("============================")
 
-#Ejerecicio 2 
+#Ejercicio 2
+
+notatp1 = float(input("Ingrese la nota del TP1: "))
+notatp2 = float(input("Ingrese la nota del TP2: "))
+notatp3 = float(input("Ingrese la nota del TP3: "))
+ep = float(input("Ingrese la nota del Examen Parcial: "))
+ef = float(input("Ingrese la nota del Examen Final: "))
+
+nota_promedio_tps = (notatp1 + notatp2 + notatp3) / 3
+promedio = (nota_promedio_tps + ep + ef) / 3
+
+print("Su promedio es:",promedio)
