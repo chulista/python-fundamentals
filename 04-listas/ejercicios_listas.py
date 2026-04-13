1ca46a37e135fb12c15d9bbaf86426aace2974a3
santtu23 <santisecjalil12@gmail.com>
2026-04-13 15:43:29 -0300

    Se realizan ejercicios de listas

---
+++ 04-listas/ejercicios_listas.py
@@ -0,0 +1,37 @@
+#Ejercicio 1 
+
+'''
+lista = [20, 50, "Curso", 'Python', 3.14]
+
+print(f"""Su lista es:
+
+{lista}""")
+
+dato1 = input("Ingrese el dato que desee en la posicion 1: ")
+dato2 = input("Ingrese el dato que desee en la posicion 2: ")
+
+lista[0] = dato1
+lista[1] = dato2
+
+print(f"""Su lista actualizada es:
+
+{lista}""")
+'''
+
+#Ejercicio 2
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros)
+print("========================")
+
+print(f"{numeros[4]} x 2 = {numeros[4]*2}")
+print(f"{numeros[7]} x 2 = {numeros[7]*2}")
+print(f"{numeros[9]} x 2 = {numeros[9]*2}")
+
+numeros[4] = numeros[4]*2
+numeros[7] = numeros[7]*2
+numeros[9] = numeros[9]*2
+
+print("========================")
+print(numeros)
