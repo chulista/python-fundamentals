7a91eb54f7c6360039f9f5263567d9086009f3b5
santtu23 <santisecjalil12@gmail.com>
2026-04-15 23:32:20 -0300

    Doy incio a un segudno ejercicio para el parcial

---
+++ examen/2do_pdf_parcial.py
@@ -0,0 +1,19 @@
+#Caso del hotel, habitaciones y estado (0 = libre / 1 = ocupada)
+
+habitaciones = []
+estados = []
+
+while True:
+        print("""    =========MENU DEL HOTEL=========
+        1. Ingresar números de habitación
+        2. Ingresar estados (0/1) paralelos
+        3. Mostrar estado general  
+        4. Consultar estado de una habitación
+        5. Listar ocupadas o libres (según lo que se pida)
+        6. Agregar habitación
+        7. Cambiar estado
+        8. Salir  
+        """)
+
+        opcion = ("Ingrese la opcion que desee: ")
+        
