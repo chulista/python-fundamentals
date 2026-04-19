af9c8af77de15d5b94a53816d92e32b8221a3c02
santtu23 <santisecjalil12@gmail.com>
2026-04-19 12:09:50 -0300

    Empiezo una nueva practica, tome tiempo esta vez y tarde 1 hora con 15 minutos en hacer los primeros 4 casos

---
+++ examen/caso_taller.py
@@ -0,0 +1,102 @@
+ordenes = []
+horas = []
+
+while True:
+    print("""\n===MENU DEL TALLER===
+
+1: Ingrese ordenes.
+2: Ingresar horas estimadas por orden.
+3: Mostrar agenda del taller.
+4: Consultar horas por orden.
+5: Listar ordenes con 0 horas
+6: Agregar orden.
+7: Actualizar horas.
+8: Salir.""")
+    
+    opcion = input("\nIngrese la opcion que desee: ")
+
+    match opcion:
+        case "1":
+            while True: 
+                cantidad = input("Ingrese la cantidad ordenes que desea ingresar: ")
+                if not cantidad.isdigit():
+                    print("Debe ingresar numeros mayores a '0'")
+                    continue
+                elif cantidad == "0":
+                    print("Debe ingresar numeros mayores a '0'")
+                    continue
+                cantidad = int(cantidad)
+                break
+                
+            for i in range(cantidad):
+                while True:
+                    numero_orden = input(f"Ingrese el numero de orden {i+1}: ")
+                    if not numero_orden.isdigit():
+                        print("Debe ingresar numeros mayores a '0'")
+                        continue
+                    elif numero_orden == "0":
+                        print("Debe ingresar numeros mayores a '0'")
+                        continue
+                    elif f"ORD-{numero_orden}" in ordenes:
+                        print("Esa orden ya esta ingresada")
+                    else:
+                        ordenes.append(f"ORD-{numero_orden}")
+                        horas.append(0)
+                        print("Ordenes creadas correctamente")
+                        break
+                              
+        case "2":
+
+            if not ordenes:
+                print("No hay ordenes ingresadas")
+                continue
+            for i in range(len(ordenes)):
+                while True:
+                    ingreso_horas = input(f"Ingrese la cantidad de horas para {ordenes[i]}: ")
+                    if not ingreso_horas.isdigit():
+                        print("Debe ingresar numeros enteros mayores a '0'")
+                        continue
+                    elif ingreso_horas == "0":
+                        print("Debe ingresar numeros enteros mayores a '0'")
+                        continue
+                    ingreso_horas = float(ingreso_horas)
+                    
+                    horas[i] = ingreso_horas
+                    print("Horas agregadas correctamente")
+                    break
+
+        case "3":
+
+            if not ordenes:
+                print("No hay ordenes ingresadas")
+                continue
+            print("===AGENDA DEL TALLER===")
+            for i in range(len(ordenes)):
+                print(f"La {ordenes[i]} tiene {horas[i]} horas")
+  
+        case "4":
+
+            if not ordenes:
+                print("No hay ordenes ingresadas")
+                continue
+            consulta = input("Ingrese la orden que desee consultar, ej; ORD-numero: ").strip()
+            if consulta not in ordenes:
+                print("La orden no existe")
+            else:
+                i = ordenes.index(consulta)
+                print(f"Orden {ordenes[i]} | Tiempo estimado {horas[i]}")
+
+        case "5":
+            pass
+        case "6":
+            pass
+        case "7":
+            pass
+        case "8":
+            print("Saliendo de la aplicacion...")
+            break
+        case _:
+            print("Opcion incorrecta...")
+
+
+    
